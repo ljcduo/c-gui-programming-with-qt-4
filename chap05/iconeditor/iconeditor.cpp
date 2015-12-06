8a707e98628589f9a11b1dd9c8150761255d2794
ljcduo <501297819@qq.com>
2015-12-06 22:17:34 +0800

    fix Qt5 against using QRegion::intersect

--- chap05/iconeditor/iconeditor.cpp
+++ chap05/iconeditor/iconeditor.cpp
@@ -84,7 +84,7 @@ void IconEditor::paintEvent(QPaintEvent *event)
     for (int i = 0; i < image.width(); ++i) {
         for (int j = 0; j < image.height(); ++j) {
             QRect rect = pixelRect(i, j);
-            if (!event->region().intersect(rect).isEmpty()) {
+            if (!event->region().intersected(rect).isEmpty()) {
                 QColor color = QColor::fromRgba(image.pixel(i, j));
                 if (color.alpha() < 255)
                     painter.fillRect(rect, Qt::white);
